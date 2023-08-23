61695a162b31f1a5e398c8c4d9cdeeeb800dfaed
Lurrégia Barreto Adão <luhhbarreto@gmail.com>
2023-08-23 11:49:04 -0300

    adcionar segundo exercicio

---
+++ exercicio2.go
@@ -0,0 +1,24 @@
+package main
+
+/*
+- Use var para declarar três variáveis. Elas devem ter package-level scope. Não atribua valores a estas variáveis. Utilize os seguintes identificadores e tipos para estas variáveis:
+    1. Identificador "x" deverá ter tipo int
+    2. Identificador "y" deverá ter tipo string
+    3. Identificador "z" deverá ter tipo bool
+- Na função main:
+    1. Demonstre os valores de cada identificador
+    2. O compilador atribuiu valores para essas variáveis. Como esses valores se chamam?
+*/
+
+import "fmt"
+
+// Declaração de variáveis com package-level scope
+var x int
+var y string
+var z bool
+
+func Exercicio2() {
+
+	// Imprime as variáveis mostrando seus valores com quebras de linha
+	fmt.Printf("%v \n%v \n%v", x, y, z)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module AprendendoGo
+module main
 
 go 1.21.0
