efe4b6b0cd53df3c3eb2e35ca9a6fd296249277d
Lurrégia Barreto Adão <luhhbarreto@gmail.com>
2023-08-30 08:06:16 -0300

    adicionar exercicio 9

---
+++ exercicio9.go
@@ -0,0 +1,19 @@
+/*
+ Crie um programa que:
+    - Atribua um valor int a uma variável
+    - Demonstre este valor em decimal, binário e hexadecimal
+    - Desloque os bits dessa variável 1 para a esquerda, e atribua o resultado a outra variável
+    - Demonstre esta outra variável em decimal, binário e hexadecimal
+*/
+
+package main
+
+import "fmt"
+
+func Exercicio9() {
+	x := 150
+	fmt.Printf("DECIMAL: %d\t BINARY: %b\t HEXADECIMAL: %#x\n", x, x, x)
+
+	y := x >> 1
+	fmt.Printf("DECIMAL: %d\t BINARY: %b\t HEXADECIMAL: %#x", y, y, y)
+}
