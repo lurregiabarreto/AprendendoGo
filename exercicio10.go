3fbe6237589530a3dba9ed052093b0b758b1ac8b
Lurrégia Barreto Adão <luhhbarreto@gmail.com>
2023-08-31 09:21:10 -0300

    adicionar exercicio 10

---
+++ exercicio10.go
@@ -0,0 +1,16 @@
+/*
+- Crie uma variável de tipo string utilizando uma raw string literal.
+- Demonstre-a.
+*/
+package main
+
+import "fmt"
+
+func Exercicio10() {
+	x := `como 
+			é bom 
+			  poder aprender
+		 	    Golang
+			`
+	fmt.Println(x)
+}
